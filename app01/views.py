5a15559b2d9e5b776d91674bcef12a3ae16a8552
wangjs-github <17863961465@163.com>
2022-12-27 17:02:52 +0800

    bootstrap

--- app01/views.py
+++ app01/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, reverse
+import requests
 
 
 # Create your views here.
@@ -8,6 +9,9 @@ def index(request):
     return HttpResponse('你好')
 
 
-def news(request):
-
-    return render(request, "news_index.html", {'name': 'QS'})
+def news(req):
+    url = 'http://192.168.50.215:8081//admin/device/deviceCount/getNumberEarlier'
+    res = requests.get(url)
+    res_json = res.json()
+    data = res_json['data']
+    return render(req, "news/news_index.html", {'name': 'QS', 'data': data})
